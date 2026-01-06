264b85fdce1091bd526fd8b39feef5756a11e0b1
tushar vashishtha <vashishthatushar004@gmail.com>
2026-01-06 12:58:26 +0530

    sdf

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://chatai-123g.onrender.com",
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
   withCredentials: true
 });
 
